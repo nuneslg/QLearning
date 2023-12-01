7661d4e36d6c057d191bc6134f5f660e07edf7cc
nuneslg <lgnunees@gmail.com>
2023-12-01 13:00:32 -0300

    add first connection

--- client.py
+++ client.py
@@ -1,1 +1,8 @@
-#Aqui vocês irão colocar seu algoritmo de aprendizado
+import connection as cn
+
+socket = cn.connect(2037)
+
+state, reward = cn.get_state_reward(socket, 'jump') #connection test
+
+print('state: ', state)
+print('reward: ', reward)
